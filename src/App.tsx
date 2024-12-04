8d350e977af784816fc4e74969f2cfe5fe709759
andresurbano <86920626+andresurbano@users.noreply.github.com>
2024-12-04 01:29:05 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { Route, Redirect } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login'; // Ensure you have this file created or remove this line if not needed
-import './App.css';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar 
+} from '@ionic/react';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route path="/login" component={Login} exact={true} />
-        <Redirect exact from="/" to="/home" />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Inicio</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <div className="ion-padding">
+          <h1>Bienvenido a la aplicación</h1>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
 
-export default App;
+export default Home;
